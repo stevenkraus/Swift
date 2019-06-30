89eadd2709a043aeaa11be7bb3b8529707abe640
stevenkraus <52385833+stevenkraus@users.noreply.github.com>
2019-06-30 15:02:51 -0700

    Update SynchronizedDisposeType.swift

--- Concurrency/SynchronizedDisposeType.swift
+++ Concurrency/SynchronizedDisposeType.swift
@@ -2,8 +2,8 @@
 //  SynchronizedDisposeType.swift
 //  RxSwift
 //
-//  Created by Krunoslav Zaher on 10/25/15.
-//  Copyright © 2015 Krunoslav Zaher. All rights reserved.
+//  Created by  Steven Kraus on 10/25/15.
+//  Copyright © 2015 Steven Kraus. All rights reserved.
 //
 
 protocol SynchronizedDisposeType : class, Disposable, Lock {
